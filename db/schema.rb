daebbb892c9e9218e96cd4185f7f8813e51e15a0
mbd-s <michaelbsilverberg@gmail.com>
2016-05-04 09:48:00 -0700

    added author to recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504163205) do
+ActiveRecord::Schema.define(version: 20160504164555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160504163205) do
     t.string   "posted"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "author"
   end
 
 end
