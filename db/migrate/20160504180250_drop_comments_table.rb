3230830d06efe92a701cf2d5b6b50519dfc66c81
mbd-s <michaelbsilverberg@gmail.com>
2016-05-04 11:05:27 -0700

    dropped comments table

---
+++ db/migrate/20160504180250_drop_comments_table.rb
@@ -0,0 +1,10 @@
+class DropCommentsTable < ActiveRecord::Migration
+  def up
+    drop_table :comments
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504175508) do
+ActiveRecord::Schema.define(version: 20160504180250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "recipe_id"
-  end
-
-  add_index "comments", ["recipe_id"], name: "index_comments_on_recipe_id", using: :btree
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -44,5 +36,4 @@ ActiveRecord::Schema.define(version: 20160504175508) do
     t.datetime "posted"
   end
 
-  add_foreign_key "comments", "recipes"
 end
